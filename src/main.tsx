8cb1ea68c2a29ebd4283e4a8c2cc3a28c0a4181d
Eduard Tesvintsev <tesvintsev@gmail.com>
2023-10-31 20:22:33 +0300

    Data Request And Error Handling

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,11 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Menu } from './pages/Menu/Menu.tsx';
-import { Error } from './pages/Error/Error.tsx';
+import { Error as ErropPage } from './pages/Error/Error.tsx';
 import { Layout } from './layout/Menu/Layout.tsx';
+import { Product } from './pages/Product/Product.tsx';
+import axios from 'axios';
+import { PREFIX } from './helpers/API.ts';
 
 const router = createBrowserRouter([
 	{
@@ -19,12 +22,26 @@ const router = createBrowserRouter([
 			{
 				path: '/cart',
 				element: <Cart />
+			},
+			{
+				path: '/product/:id',
+				element: <Product />,
+				errorElement: <>Ошибка</>,
+				loader: async ({ params }) => {
+					await new Promise<void>((resolve) => {
+						setTimeout(() => {
+							resolve();
+						}, 2000);
+					});
+					const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
+					return data;
+				}
 			}
 		]
 	},
 	{
 		path: '*',
-		element: <Error />
+		element: <ErropPage />
 	}
 ]);
 
