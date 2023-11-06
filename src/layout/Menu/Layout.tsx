a8848f5d9762868775b081c335261bb889f1adde
Eduard Tesvintsev <tesvintsev@gmail.com>
2023-11-07 00:18:26 +0300

    shopping cart

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -11,6 +11,7 @@ export function Layout() {
 	const navigate = useNavigate();
 	const dispatch = useDispatch<AppDispath>();
 	const profile = useSelector((s: RootState) => s.user.profile);
+	const items = useSelector((s: RootState) => s.cart.items);
 
 	useEffect(() => {
 		dispatch(getProfile());
@@ -37,7 +38,8 @@ export function Layout() {
 				<NavLink to='/cart' className={({ isActive }) => cn(styles['link'], {
 					[styles.active]: isActive
 				})}>
-					<img src="/cart-icon.svg" alt="Иконка корзины" />Корзина</NavLink>
+					<img src="/cart-icon.svg" alt="Иконка корзины" />Корзина <span className={styles['cart-count']}>{items.reduce((acc, item) => acc += item.count, 0)}</span></NavLink>
+
 			</div>
 			<Button className={styles['exit']} onClick={logout}>
 				<img src="/exit-icon.svg" alt="Иконка выхода" />
